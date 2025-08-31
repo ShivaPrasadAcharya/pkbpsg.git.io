514e9d9b9a4164d9690f705657f02b9aced14a53
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-31 10:26:28 +0545

    Update data2Images.js

--- data2Images.js
+++ data2Images.js
@@ -1,11 +1,11 @@
 // data2Images.js - Images Dataset
 var data2Images = `ID,Title,Description,Image
-000, अकलादेवी,अकलादेव मन्दिर-लामाचौर,IMAGES/akaladevi3.jpg
-001, Group Photo,BPSG-3rd Batch, IMAGES/groupphoto.jpg
-002, Gurupurnima-2081,BPSG-2nd Batch,IMAGES/groupphoto2.jpg
-003, Group Photo,BPSG-2nd Batch,IMAGES/groupphoto1.jpg
-003, With Coordinator,Birthday Celebration of Loknath Dulal sir (bpsg coordinator),IMAGES/withbpsgcoordinator.jpg
-004,शिवप्रसाद आचार्य,PSMII-Teacher,IMAGES/myphoto.jpg
+000, अकलादेवी,अकलादेव मन्दिर-लामाचौर,Images/akaladevi3.jpg
+001, Group Photo,BPSG-3rd Batch, Images/groupphoto.jpg
+002, Gurupurnima-2081,BPSG-2nd Batch,Images/groupphoto2.jpg
+003, Group Photo,BPSG-2nd Batch,Images/groupphoto1.jpg
+003, With Coordinator,Birthday Celebration of Loknath Dulal sir (bpsg coordinator),Images/withbpsgcoordinator.jpg
+004,शिवप्रसाद आचार्य,PSMII-Teacher,Images/myphoto.jpg
 `;
 
 // Dataset metadata
@@ -16,6 +16,7 @@ var data2ImagesInfo = {
     columns: 4,
     primaryKey: "ID"
 };
+
 
 
 
