9b414dcaf78ab6e3b822853d50efe75794acfa94
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-01 19:40:15 +0545

    content updated.

--- data3Links.js
+++ data3Links.js
@@ -6,7 +6,7 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 005,Past Questions,BPSG 7th sem-PSMII Past Question,html,HTML/pastquestions.htm
 006,Contacts,BPSG 7th sem-2082 contact list,html,HTML/contactlist.html
 007,Answers,Topper Paper-PSMII-2081 Ist schedule exam,pdf,DOCS/bestanswerpsm2081.pdf
-007a,Best Answers,Topper Paper-PSMII-2082 PreBoard,pdf,DOCS/bestanswerofpreboard2082.pdf
+007a,Best Answers,Jeeya Shakya's Answer Paper-PSMII-2082 PreBoard,pdf,DOCS/bestanswerofpreboard2082.pdf
 008,Answers,Topper Paper-PSMII-2082 IInd schedule exam,pdf,DOCS/TopperPaper2.pdf
 009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
 010,Notes,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
