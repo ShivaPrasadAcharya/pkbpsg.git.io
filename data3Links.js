a415e45e79124ed761dcce49ce4a6f86b22d1a56
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-02 08:01:25 +0545

    content updated

--- data3Links.js
+++ data3Links.js
@@ -2,12 +2,12 @@
 var data3Links = `ID,Categry,Subject,Remark,Link
 002,Syllables,BPSG 7th sem-PSMII syllables,html,HTML/psm2syllablesbpsg.htm
 003,Syllables,BPSG 7th sem-PSMII syllables,html,HTML/psm2syllablesbpsg.htm
-004,Past Questions,BPSG 7th sem-PSMII Past Question,html,DOCS/pastquestions.pdf
+004,Past Questions,BPSG 7th sem-PSMII Past Question,pdf,DOCS/pastquestions.pdf
 005,Past Questions,BPSG 7th sem-PSMII Past Question,html,HTML/pastquestions.htm
 006,Contacts,BPSG 7th sem-2082 contact list,html,HTML/contactlist.html
-007,Answers,Topper Paper-PSMII-2081 Ist schedule exam,pdf,DOCS/bestanswerpsm2081.pdf
-007a,Best Answers,Jeeya Shakya's Answer Paper-PSMII-2082 PreBoard,pdf,DOCS/bestanswerofpreboard2082.pdf
-008,Answers,Topper Paper-PSMII-2082 IInd schedule exam,pdf,DOCS/TopperPaper2.pdf
+007a,Answers Sheet-PSMII,Topper Paper-Ist schedule exam 2081 ,pdf,DOCS/bestanswerpsm2081.pdf
+007b,Best Answers-PSMII,Jeeya's Answer Sheet-PreBoard-2082,pdf,DOCS/bestanswerofpreboard2082.pdf
+007c,Answers Sheet,Dikshya Paper-2082 IInd schedule exam,pdf,DOCS/secondtestanswersheet2082.pdf
 009,Notes,Global Development Lab of USA,html,HTML/GDL-USA.htm
 010,Notes,patient innovation platform of portugal,html,HTML/patientinnovation.htm
 011,PPT by Dikshya,population registry of estonia,pdf,DOCS/populationregistryestonia.pdf
@@ -15,7 +15,7 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 013,PPT by Divya,E-Waste recycling of China,pdf,DOCS/ewasterecyclingofchinabydivya.pdf
 014,PPT by Archana,X-Road of Estonia,pdf,DOCS/XroadEstoniabyarchana.pdf
 015,PPT by Pratikshya,Intelligent Transport System,pdf,DOCS/intelligenttransportbypratikshya.pdf
-016,PPT byJamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
+016,PPT by Jamuna,e-police,pdf,DOCS/epolicebyjamuna.pdf
 `;
 // Dataset metadata
 var data3LinksInfo = {
