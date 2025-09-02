718e56184060089823078360e014074c3b2905f1
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-02 09:35:27 +0545

    Update data8html1.js

--- data8html1.js
+++ data8html1.js
@@ -1,14 +1,11 @@
 // data8html1.js
 window.html1Content = `
-<h1 id="civil-law-i-general-principles-and-provisions-of-contract-">Civil1 Law-I (General Principles and Provisions of Contract)</h1>
-<p> pokhara </p>
-<h1>shivam</h1>
-<p> kathmandu </p>
-<h1> ok ok</h1>
-<p> okokok </p>
+<h1 id="civil-law-i-general-principles-and-provisions-of-contract-">Best of Luck for your Board Exam.</h1>
+<p> You are your own competitor. Be Better than Before.</p>
 `;
 window.html1Meta = { 
-  name: "Some HTML",
+  name: "no content",
    emoji: "📝" 
 
   };
+
