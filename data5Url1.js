55397a93078f2f2060024bd7060b173b81bde827
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-05 07:03:50 +0545

    Update data5Url1.js

--- data5Url1.js
+++ data5Url1.js
@@ -3,7 +3,7 @@ var data5Url1 = `sno,subject,description,shorturl,longurl
 02ac,past questions,BPSG 7th sem-PSMII Past Question,see Past Questions-docx,DOCS/pastquestions.docx
 02b,git-Link,HTML,index.html,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/index.html
 02c,git-Link,Images,data2Images.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data2Images.js
-02d,git-Link,URL,data5Url.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data5Url.js
+02d,git-Link,URL,data5Url.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data5Url1.js
 02e,git-Link,Students,data1Trainees.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data1Trainees.js
 02f,git-Link,Status,data4Status.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data4Status.js
 02g,git-Link,Links,data3Links.js,https://github.com/ShivaPrasadAcharya/pkbpsg.git.io/edit/main/data3Links.js
@@ -23,5 +23,6 @@ var data5Url1Info = {
               "Notes": "blue"
     }
 };
+
 
 
