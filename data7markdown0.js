e67896dab74867479bc9edc3e08a28f7adb2949e
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-12-04 07:14:14 +0000

    ok

---
+++ data7markdown0.js
@@ -0,0 +1,12 @@
+// data7markdown1.js
+//find & replace: ```.*?\n([\s\S].*?)```  >> <pre>$1</pre>
+window.markdown0Content = `
+
+
+`;
+window.markdown0Meta = { 
+  name: "sample page",
+   emoji: "📄"
+
+   };
+
